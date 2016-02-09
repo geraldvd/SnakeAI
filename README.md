41215667f60027ba65f33c3839c9bc3b5e2000b5
Gerald van Dalen <geraldplus@gmail.com>
2016-02-09 16:42:10 +0100

    empty project (usable for other projects as well)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Snake AE
+
+* Program simple snake game (two color; e.g., using OpenCV)
+* Program ML/RL algorithm to play it (idea from: Deepmind)
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "config.h"
+
+// Include standard libraries
+
+#ifdef WITH_QT
+    #include <QApplication>
+#else
+#endif
+
+// Include project files
+
+// Namespaces
+using namespace std;
+
+int main(int argc, char **argv) {
+    return 0;
+}
